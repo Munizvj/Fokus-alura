4cd71a6e25e63ba1c9dcf9e565e7a01b59aa2020
Munizvj <joaomuniz319@gmail.com>
2026-08-18 16:46:24 -0300

    Initial project for the course on element manipulation and LocalStorage.

--- Fokus/script.js
+++ Fokus/script.js
@@ -14,9 +14,9 @@ const tempoNaTela = document.querySelector('#timer')
 const musica = new Audio('.//sons/luna-rise-part-one.mp3')
 const audioPlay = new Audio('.//sons/play.wav');
 const audioPausa = new Audio('.//sons/pause.mp3');
-const audioTempoFinalizado = new Audio('.//sons/beep.mp3')
+const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 
-let tempoDecorridoEmSegundos = 1500
+let tempoDecorridoEmSegundos = 30
 let intervaloId = null
 
 musica.loop = true
