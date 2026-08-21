803a15e048ef591b392ce66b01adfcc772c0b657
Munizvj <joaomuniz319@gmail.com>
2026-08-21 11:21:54 -0300

    bugFix/tarefa-completa corrigido

--- Fokus/script-crud.js
+++ Fokus/script-crud.js
@@ -1,5 +1,3 @@
-// encontrar o botão adicionar tarefa
-
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 const textarea = document.querySelector('.app__form-textarea')
@@ -35,9 +33,7 @@ function criarElementoTarefa(tarefa) {
     botao.classList.add('app_button-edit')
 
     botao.onclick = () => {
-        // debugger
         const novaDescricao = prompt("Qual é o novo nome da tarefa?")
-        // console.log('Nova descrição da tarefa: ', novaDescricao)
         if (novaDescricao) {            
             paragrafo.textContent = novaDescricao
             tarefa.descricao = novaDescricao
@@ -46,29 +42,34 @@ function criarElementoTarefa(tarefa) {
     }
 
     const imagemBotao = document.createElement('img')
-    imagemBotao.setAttribute('src', '/imagens/edit.png')
+    imagemBotao.setAttribute('src', './imagens/edit.png')
     botao.append(imagemBotao)
 
     li.append(svg)
     li.append(paragrafo)
     li.append(botao)
 
-    li.onclick = () => {
-        document.querySelectorAll('.app__section-task-list-item-active')
-            .forEach(elemento => {
-                elemento.classList.remove('app__section-task-list-item-active')
-            })
-        if (tarefaSelecionada == tarefa) {
-            paragrafoDescricaoTarefa.textContent = ''
-            tarefaSelecionada = null
-            liTarefaSelecionada = null
-            return
+    if(tarefa.completa){
+        li.classList.add('app__section-task-list-item-complete')
+        botao.setAttribute('disabled', 'disabled')
+    }else{
+        li.onclick = () => {
+            document.querySelectorAll('.app__section-task-list-item-active')
+                .forEach(elemento => {
+                    elemento.classList.remove('app__section-task-list-item-active')
+                })
+            if (tarefaSelecionada == tarefa) {
+                paragrafoDescricaoTarefa.textContent = ''
+                tarefaSelecionada = null
+                liTarefaSelecionada = null
+                return
+            }
+            tarefaSelecionada = tarefa
+            liTarefaSelecionada = li
+            paragrafoDescricaoTarefa.textContent = tarefa.descricao
+    
+            li.classList.add('app__section-task-list-item-active')
         }
-        tarefaSelecionada = tarefa
-        liTarefaSelecionada = li
-        paragrafoDescricaoTarefa.textContent = tarefa.descricao
-
-        li.classList.add('app__section-task-list-item-active')
     }
 
     return li
@@ -101,5 +102,7 @@ document.addEventListener('FocoFinalizado', () => {
         liTarefaSelecionada.classList.remove('app__section-task-list-item-active')
         liTarefaSelecionada.classList.add('app__section-task-list-item-complete')
         liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'disabled')
+        tarefaSelecionada.completa = true
+        atualizarTarefas()
     }
 })
